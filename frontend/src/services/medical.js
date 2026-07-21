ac7872773ea1125ffa02b693d414d90f802cfe5c
sakshi3861 <sakshisushilshelar@gmail.com>
2026-07-21 22:09:03 +0530

    Removed frontend folder from feature/news-risk-engine

--- frontend/src/services/medical.js
+++
@@ -1,6 +0,0 @@
-import { profileService } from './profile';
-
-export const medicalService = {
-  get: async () => (await profileService.getProfile()).medical,
-  update: changes => profileService.updateMedical(changes),
-};
